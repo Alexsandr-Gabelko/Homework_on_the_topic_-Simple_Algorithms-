d36b46931788d3071e6600ca32cd9b74ada95f1a
Aleksandr Gabelko <aleksandrgabelko@gmail.com>
2024-01-29 21:28:08 +0600

    Рабоыий код решения первой задачии

--- Задача 1/Program.cs
+++ Задача 1/Program.cs
@@ -2,4 +2,16 @@
 на вход число и проверяет, кратно ли оно
 одновременно 7 и 23 */
 
+Console.Write("Введите число: ");
+string number = Console.ReadLine()!;
+int int_number = int.Parse(number);
+
+if (int_number % 7 == 0 && int_number % 23 == 0)  
+    {
+    Console.WriteLine("да");
+    }
+else 
+    {
+    Console.WriteLine("нет");
+    }
 
