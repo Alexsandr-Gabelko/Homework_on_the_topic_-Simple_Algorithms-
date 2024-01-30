645960932e1698fc2a5b2e043e66565065e4dc53
Aleksandr Gabelko <aleksandrgabelko@gmail.com>
2024-01-30 17:51:49 +0600

    Рабоыий код решения второй задачии

--- Задача 2/Program.cs
+++ Задача 2/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*    Напишите программу, которая принимает
+на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠
+0 и выдаёт номер координатной четверти плоскости,
+в которой находится эта точка.                  */
+
+Console.WriteLine("Введите координаты точки (X и Y), причём X и Y не равны 0 ");
+
+Console.Write("Введите координату Х : ");
+string X = Console.ReadLine()!;
+int int_X = int.Parse(X);
+
+Console.Write("Введите координату Y : ");
+string Y = Console.ReadLine()!;
+int int_Y = int.Parse(Y);
+
+if (int_X > 0 && int_Y > 0)
+    Console.WriteLine("1");  
+
+if (int_X < 0 && int_Y > 0)
+    Console.WriteLine("2");  
+
+if (int_X < 0 && int_Y < 0)
+    Console.WriteLine("3");
+
+if (int_X > 0 && int_Y < 0)
+    Console.WriteLine("4");  
