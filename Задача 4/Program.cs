2913f31410e7addbc9eb2f1983a4bbf82371b732
Aleksandr Gabelko <aleksandrgabelko@gmail.com>
2024-01-30 21:14:21 +0600

    Рабочий код решения четвертой задачии

--- Задача 4/Program.cs
+++ Задача 4/Program.cs
@@ -1,3 +1,36 @@
 ﻿/*   Напишите программу, которая на вход
 принимает натуральное число N, а на выходе
 показывает его цифры через запятую.         */
+
+Console.Write("Введите число: ");
+string number = Console.ReadLine()!;
+int int_number = int.Parse(number);
+
+int index = 0;
+int num = int_number;
+
+while (int_number > 0)
+{
+    int_number = int_number / 10;
+    index += 1;
+}
+
+// Console.WriteLine(index);
+
+int raz = 10;
+
+for (int i = 2; i < index; i++)
+{
+    raz *= 10; 
+}
+
+//  Console.WriteLine(raz);
+
+for (int i = 1; i < index; i++)
+{
+   Console.Write(num / raz);
+   Console.Write(", ");
+   num = num - num / raz * raz;
+   raz /=10; 
+}
+Console.Write(num);
