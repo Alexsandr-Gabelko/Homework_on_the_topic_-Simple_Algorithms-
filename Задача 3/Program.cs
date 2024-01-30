ca1b2b8ca7141c9a17c1d53fcb8f69969502fb4a
Aleksandr Gabelko <aleksandrgabelko@gmail.com>
2024-01-30 19:41:35 +0600

    Рабочий код решения третьей задачии

--- Задача 3/Program.cs
+++ Задача 3/Program.cs
@@ -1,3 +1,15 @@
 ﻿/*    Напишите программу, которая принимает
 на вход целое число из отрезка [10, 99] и показывает
 наибольшую цифру числа.                  */
+
+Console.Write("Введите число: ");
+string number = Console.ReadLine()!;
+int int_number = int.Parse(number);
+
+int number1 = int_number / 10;
+int number2 = int_number % 10;
+
+if (number1 > number2)
+    Console.Write(number1); 
+else
+    Console.Write(number2);
